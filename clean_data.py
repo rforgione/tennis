c871e034ce278464891a38cbe2d706fc9e006518
Robert Forgione <rforgione@gmail.com>
2014-08-13 13:46:00 -0400

    Built out X and y matrices; imputed missing values using the column means

--- clean_data.py
+++ clean_data.py
@@ -1,37 +1,50 @@
 import numpy as np
 import pandas as pd
+from pandas import DataFrame, Series
 import sklearn
+from sklearn.preprocessing import Imputer
 
-data_files = ['AusOpen-men-2013.csv', 'AusOpen-women-2013.csv', 'FrenchOpen-men-2013.csv',
-			'FrenchOpen-women-2013.csv', 'USOpen-men-2013.csv', 'Wimbledon-men-2013.csv',
-			'Wimbledon-women-2013.csv']
+def clean_data(*args):
 
-all_data = np.array([[]])
+	data_files = args
 
-for file in data_files:
-	new_data = pd.read_csv('data/%s' % file)
+	for file in data_files:
+		new_data = pd.read_csv('data/%s' % file)
 
-	# We can actually create two data sets out of each
-	# tournament result rather than the one provided by the data
+		# We can actually create two data sets out of each
+		# tournament result rather than the one provided by the data
 
-	X_1 = np.array(new_data.loc[:,'FSP.1':'ST5.1'].copy())
-	y_1 = np.array([new_data['Result'].copy()]).reshape(len(new_data['Result']),1)
+		X_1 = np.array(new_data.loc[:,'FSP.1':'ST5.1'].copy())
+		y_1 = np.array([new_data['Result'].copy()]).reshape(len(new_data['Result']),1)
 
-	X_2 = np.array(new_data.loc[:,'FSP.2':'ST5.2'].copy())
-	# multiply by one to store the data as binary rather than boolean
-	y_2 = np.array((new_data['Result'] == 0).copy()).reshape(len(new_data['Result']),1)
+		X_2 = np.array(new_data.loc[:,'FSP.2':'ST5.2'].copy())
+		# multiply by one to store the data as binary rather than boolean
+		y_2 = np.array((new_data['Result'] == 0).copy()).reshape(len(new_data['Result']),1)
 
-	data_1 = np.hstack([X_1, y_1])
-	data_2 = np.hstack([X_2, y_2])
+		X = np.vstack([X_1, X_2])
+		y = np.vstack([y_1, y_2])
 
-	data = np.vstack([data_1, data_2])
+		try:
+			all_X = np.vstack([all_X, X])
+			all_y = np.vstack([all_y, y])
+		except NameError:
+			all_X, all_y = X, y
 
-	try:
-		all_data = np.vstack([all_data, data])
-	except ValueError:
-		all_data = data
+	return (all_X, all_y)
 
-print all_data.shape
+def impute_matrix(matrix):
+	imputer = Imputer(missing_values='NaN', strategy='mean', axis=0, copy=True)
+	new_matrix = imputer.fit_transform(matrix)
+	return new_matrix
 
+if __name__ == "__main__":
+
+	X, y = clean_data('AusOpen-men-2013.csv', 'AusOpen-women-2013.csv', 'FrenchOpen-men-2013.csv',
+					'FrenchOpen-women-2013.csv', 'USOpen-men-2013.csv', 'Wimbledon-men-2013.csv',
+					'Wimbledon-women-2013.csv')
+
+	# 4762 NA values; we remove these through mean imputation
+	X = impute_matrix(X)
+	
 
 
